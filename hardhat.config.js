cb8f72f8b8db5dd698e651c665540c96d94b8ebe
Jumcee <futhmah456@gmail.com>
2023-12-16 14:19:42 +0100

    BuyMeACoffee

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { PRIVATE_KET, API_URL } = process.env
 module.exports = {
   solidity: "0.8.0",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.API_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
 
--- scripts/buy-coffee.js
+++ scripts/buy-coffee.js
@@ -11,7 +11,7 @@ async function printBalances(addresses) {
   let idx = 0;
   for (const address of addresses) {
     console.log(`Address ${idx} balance: `, await getBalance(address));
-    idx ++;
+    idx++;
   }
 }
 
@@ -27,46 +27,54 @@ async function printMemos(memos) {
 }
 
 async function main() {
-  // Get the example accounts we'll be working with.
-  const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
+  try {
+    // Get the example accounts we'll be working with.
+    const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
 
-  // We get the contract to deploy.
-  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
-  const buyMeACoffee = await BuyMeACoffee.deploy();
+    // We get the contract to deploy.
+    const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+    const buyMeACoffee = await BuyMeACoffee.deploy();
 
-  // Deploy the contract.
-  await buyMeACoffee.deployed();
-  console.log("BuyMeACoffee deployed to:", buyMeACoffee.address);
+    // Deploy the contract.
+    if (buyMeACoffee && buyMeACoffee.address) {
+      console.log("BuyMeACoffee deployed to:", buyMeACoffee.address);
 
-  // Check balances before the coffee purchase.
-  const addresses = [owner.address, tipper.address, buyMeACoffee.address];
-  console.log("== start ==");
-  await printBalances(addresses);
+      // Check balances before the coffee purchase.
+      const addresses = [owner.address, tipper.address, buyMeACoffee.address];
+      console.log("== start ==");
+      await printBalances(addresses);
 
-  // Buy the owner a few coffees.
-  const tip = {value: hre.ethers.utils.parseEther("1")};
-  await buyMeACoffee.connect(tipper).buyCoffee("Carolina", "You're the best!", tip);
-  await buyMeACoffee.connect(tipper2).buyCoffee("Vitto", "Amazing teacher", tip);
-  await buyMeACoffee.connect(tipper3).buyCoffee("Kay", "I love my Proof of Knowledge", tip);
+      // Buy the owner a few coffees.
+      const tip = { value: hre.ethers.utils.parseEther("1") };
+      await buyMeACoffee.connect(tipper).buyCoffee("Carolina", "You're the best!", tip);
+      await buyMeACoffee.connect(tipper2).buyCoffee("Vitto", "Amazing teacher", tip);
+      await buyMeACoffee.connect(tipper3).buyCoffee("Kay", "I love my Proof of Knowledge", tip);
 
-  // Check balances after the coffee purchase.
-  console.log("== bought coffee ==");
-  await printBalances(addresses);
+      // Check balances after the coffee purchase.
+      console.log("== bought coffee ==");
+      await printBalances(addresses);
 
-  // Withdraw.
-  await buyMeACoffee.connect(owner).withdrawTips();
+      // Withdraw.
+      await buyMeACoffee.connect(owner).withdrawTips();
 
-  // Check balances after withdrawal.
-  console.log("== withdrawTips ==");
-  await printBalances(addresses);
+      // Check balances after withdrawal.
+      console.log("== withdrawTips ==");
+      await printBalances(addresses);
 
-  // Check out the memos.
-  console.log("== memos ==");
-  const memos = await buyMeACoffee.getMemos();
-  printMemos(memos);
+      // Check out the memos.
+      console.log("== memos ==");
+      const memos = await buyMeACoffee.getMemos();
+      printMemos(memos);
+    } else {
+      console.log("Contract deployment failed. No contract address available.");
+    }
+  } catch (error) {
+    console.error("Error deploying the contract:", error);
+  }
 }
 
-
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
